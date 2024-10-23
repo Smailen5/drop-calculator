186ff12ea4ae83af44468ef03c22ce5c733c1ba6
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-10-23 12:50:32 +0200

    refactor: :wheelchair: migliora accessibilita
    modifico tag div in tag main

--- src/components/atoms/Layout.tsx
+++ src/components/atoms/Layout.tsx
@@ -1,7 +1,7 @@
 export const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="bg-red-500 p-4 flex justify-center items-center">
+    <main className="p-4 flex justify-center items-center flex-col gap-4">
       {children}
-    </div>
+    </main>
   );
 };
