0826c1eef88600cb35c8b83c66365ed08afdfc4e
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-10-23 12:49:23 +0200

    refactor: :construction: separa componente

---
+++ src/components/atoms/HeaderText.tsx
@@ -0,0 +1,8 @@
+export const HeaderText = () => {
+  return (
+    <p>
+      Qui puoi calcolare quanti tentativi fare per droppare qualcosa all'interno
+      di the first descendant
+    </p>
+  );
+};
